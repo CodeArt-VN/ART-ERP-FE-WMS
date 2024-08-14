941efd1a2aa9b8cfd19429780e386e913fec71dd
tan phuc <bizyoh@gmail.com>
2024-08-14 19:05:20 +0700

    Reorder Location

--- location/location.page.ts
+++ location/location.page.ts
@@ -1,3 +1,4 @@
+
 import { Component } from '@angular/core';
 import { NavController, ModalController, AlertController, LoadingController, PopoverController } from '@ionic/angular';
 import { EnvService } from 'src/app/services/core/env.service';
@@ -5,6 +6,7 @@ import { PageBase } from 'src/app/page-base';
 import { BRA_BranchProvider, WMS_LocationProvider } from 'src/app/services/static/services.service';
 import { Location } from '@angular/common';
 import { NavigationExtras } from '@angular/router';
+import { CommonService } from 'src/app/services/core/common.service';
 
 @Component({
   selector: 'app-location',
@@ -12,10 +14,12 @@ import { NavigationExtras } from '@angular/router';
   styleUrls: ['location.page.scss'],
 })
 export class LocationPage extends PageBase {
+  _items:any;
   constructor(
     public pageProvider: WMS_LocationProvider,
     public branchProvider: BRA_BranchProvider,
     public modalController: ModalController,
+    public commonService: CommonService,
     public popoverCtrl: PopoverController,
     public alertCtrl: AlertController,
     public loadingController: LoadingController,
@@ -25,7 +29,10 @@ export class LocationPage extends PageBase {
   ) {
     super();
   }
-
+  loadedData(event?: any, ignoredFromGroup?: boolean): void {
+    super.loadedData(event);
+    this._items = this.items;
+  }
   printQRCode() {
       let navigationExtras: NavigationExtras = {
         state: this.selectedItems.map((m) => {
@@ -38,4 +45,28 @@ export class LocationPage extends PageBase {
       };
       this.nav('/qr-code-label', 'forward', navigationExtras);
     }
+  isShowModal = false;
+  doReorder(reOrderedItems){
+    this._items = reOrderedItems;
+  }
+ dismissModal(isSaveChange = false){
+  this.isShowModal = false;  
+  if(isSaveChange){
+    this.items = this._items;
+    let putItems = this.items.map(i=>  ({
+      Id : i.Id,
+      RouteSequence : i.RouteSequence
+    }))
+    this.env
+    .showLoading(
+      'Please wait for a few moments',
+      this.commonService.connect('PUT','WMS/Location/UpdateRouteSequence',putItems).toPromise()
+    )
+    .then((result: any) => {
+      this.env.showMessage('Saved!','success');
+    });
+  
+
+  }
+}
 }
