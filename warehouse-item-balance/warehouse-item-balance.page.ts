d35f71fc94ab03a3fbfda765b263cd74fcbedfad
Hùng Vũ <hungvq@live.com>
2025-03-01 10:14:19 +0700

    fix

--- warehouse-item-balance/warehouse-item-balance.page.ts
+++ warehouse-item-balance/warehouse-item-balance.page.ts
@@ -10,10 +10,9 @@ import {
 	WMS_ItemProvider,
 	WMS_TransactionProvider,
 } from 'src/app/services/static/services.service';
-import QRCode from 'qrcode';
 import { ActivatedRoute, Router } from '@angular/router';
 import { FormArray, FormBuilder, Validators } from '@angular/forms';
-import { AC_PostingPeriod } from 'src/app/models/model-list-interface';
+import { lib } from 'src/app/services/static/global-functions';
 
 @Component({
 	selector: 'app-warehouse-item-balance',
@@ -22,20 +21,18 @@ import { AC_PostingPeriod } from 'src/app/models/model-list-interface';
 	standalone: false,
 })
 export class WareHouseItemBalancePage extends PageBase {
-	currentDate = new Date();
 	storerList;
 	branchList;
-	sheets = [];
-	listYear = [];
-	listPeriodCategory = [];
-	listPeriod = [];
-	notes = [];
+	periodCategoryList = [];
+	periodList = [];
+
 	constructor(
-		//public pageProvider: WMS_StockCardProvider,
 		public pageProvider: WMS_ItemBalanceProvider,
 		public itemProvider: WMS_ItemProvider,
 		public contactProvider: CRM_ContactProvider,
-		public periodProvider: AC_PeriodCategoryProvider,
+		public periodCategoryProvider: AC_PeriodCategoryProvider,
+		public periodProvider: AC_PostingPeriodProvider,
+
 		public modalController: ModalController,
 		public popoverCtrl: PopoverController,
 		public alertCtrl: AlertController,
@@ -48,141 +45,98 @@ export class WareHouseItemBalancePage extends PageBase {
 	) {
 		super();
 		this.pageConfig.isShowFeature = true;
-		// this.pageConfig.isDetailPage = true;
+
 		this.formGroup = this.formBuilder.group({
-			IDPeriod: ['', Validators.required],
 			IDBranch: ['', Validators.required],
 			IDStorer: ['', Validators.required],
-			Lines: this.formBuilder.array([]),
+			IDPeriodCategory: ['', Validators.required],
+			IDPeriod: ['', Validators.required],
 		});
 	}
 
 	preLoadData(event?: any): void {
-		Promise.all([this.contactProvider.read({ IsStorer: true }), this.periodProvider.read({ IDBranch: this.env.selectedBranch })]).then((resp: any) => {
-			if (resp[0] && resp[0].data) this.storerList = resp[0].data;
-			if (resp[1] && resp[1].data) {
-				this.listPeriodCategory = resp[1].data;
-				this.listYear = Array.from(new Map(this.listPeriodCategory.map((item) => [item.Name, item])).values()).map((d) => {
-					return { Name: d.Name, Id: d.Id };
-				});
-			}
-		});
-		this.branchList = [...this.env.branchList];
-		this.loadedData(event);
-	}
-	subItem: any = {};
-	loadData(event?: any): void {}
-
-	loadedData(event?: any, ignoredFromGroup?: boolean): void {
-		super.loadedData(event, ignoredFromGroup);
-
-		this.formGroup.enable();
-		if (!this.formGroup.get('IDBranch').value) {
-			this.getNearestWarehouse(this.env.selectedBranch);
-		}
-	}
-	changeFilter() {
-		if (this.formGroup.invalid) return;
-		this.query = this.formGroup.getRawValue();
-		this.pageConfig.showSpinner = true;
-		this.pageProvider.commonService
-			.connect('GET', 'WMS/ItemBalance', this.query)
-			.toPromise()
-			.then((rs: any) => {
-				if (rs) {
-					// this.items = rs;
-					// this.patchValue();
-				}
-			})
-			.catch((err) => {
-				this.env.showMessage(err.error?.InnerException?.ExceptionMessage || err, 'danger');
-			});
-	}
-
-	loadItemBalance() {
-		
-	}
-
-	changePeriodCategory() {
-		let postDTO = {
-			IDBranch: this.formGroup.controls.IDBranch.value,
-			IDPeriodCategory: this.formGroup.controls.IDPeriod.value
-		}
-		this.periodProvider.commonService
-			.connect('GET', 'AC/PostingPeriod', postDTO)
-			.toPromise()
+		Promise.all([this.contactProvider.read({ IsStorer: true }), this.periodCategoryProvider.read({ IgnoredBranch: true }), 
+			//this.env.searchBranch((b) => b.Type == 'Warehouse')
+		])
 			.then((resp: any) => {
-				if (resp) {
-					this.listPeriod = resp;
-				}
-			});
-	}
-	patchValue() {
-		this.formGroup.controls.Lines = new FormArray([]);
-		this.items.forEach((line) => {
-			this.addLine(line);
-		});
-	}
+				this.storerList = resp[0].data;
+				this.periodCategoryList = resp[1].data;
+				this.branchList = lib.cloneObject(this.env.branchList); resp[2];
 
-	addLine(line, markAsDirty = false) {
-		let groups = <FormArray>this.formGroup.controls.Lines;
-		let selectedItem = line._Item;
-		let group = this.formBuilder.group({
-			_IDItemDataSource: this.buildSelectDataSource((term) => {
-				return this.itemProvider.search({
-					SortBy: ['Id_desc'],
-					Take: 20,
-					Skip: 0,
-					Term: term,
-				});
-			}),
-			IDItem: [line.IDItem || 0, Validators.required],
-			OpenQuantity: [line.OpenQuantity, Validators.required],
-			ClosedQuantity: [line.ClosedQuantity, Validators.required],
-		});
-		groups.push(group);
-		group.get('_IDItemDataSource').value?.initSearch();
-	}
-	getContinuousIndex(sheetIndex: number, rowIndex: number): number {
-		let previousCount = 0;
-		// Sum up the number of items from all previous sheets
-		for (let i = 0; i < sheetIndex; i++) {
-			previousCount += this.sheets[i]?.data.length || 0;
-		}
-		return previousCount + rowIndex + 1;
-	}
+				console.log('this.branchList', this.branchList);
+				console.log('this.periodCategoryList', this.periodCategoryList);
+				console.log('this.storerList', this.storerList);
 
-	submitData(g) {
-		if (this.formGroup.invalid) return;
-	}
+				if (this.branchList.length > 0) {
+					//Get the first branch that is warehouse and index in array > selectedBranch index
+					const selectedBranchIndex = this.branchList.findIndex((b) => b.Id == this.env.selectedBranch);
+					const warehouseBranch = this.branchList.find((b, i) => b.Type == 'Warehouse' && i >= selectedBranchIndex);
 
-	getNearestWarehouse(IDBranch) {
-		let currentBranch = this.env.branchList.find((d) => d.Id == IDBranch);
-		if (currentBranch) {
-			if (currentBranch.Type == 'Warehouse') {
-				this.formGroup.get('IDBranch').setValue(currentBranch.Id);
-				return true;
-			} else {
-				let childrentWarehouse: any = this.env.branchList.filter((d) => d.IDParent == IDBranch);
-				for (let child of childrentWarehouse) {
-					if (this.getNearestWarehouse(child.Id)) {
-						return true;
+					// //Loop through the list of branches and find the first branch that is warehouse then set it disabled = false, else set it disabled = true
+					// this.branchList.forEach((b) => {
+					// 	b.disabled = b.Type != 'Warehouse';
+					// });
+					if (warehouseBranch) {
+						this.formGroup.controls.IDBranch.setValue(warehouseBranch.Id);
 					}
 				}
-			}
+
+				if (this.periodCategoryList.length > 0) {
+					//Find the current period category based on the current date and PostingDateFrom and PostingDateTo
+					const currentDate = new Date();
+					const currentPeriodCategory = this.periodCategoryList.find((pc) => {
+						const postingDateFrom = new Date(pc.PostingDateFrom);
+						const postingDateTo = new Date(pc.PostingDateTo);
+						return currentDate >= postingDateFrom && currentDate <= postingDateTo;
+					});
+					if (currentPeriodCategory) {
+						this.formGroup.controls.IDPeriodCategory.setValue(currentPeriodCategory.Id);
+					}
+
+					this.onChangePeriodCategory();
+				}
+
+				super.loadedData(event);
+			})
+			.catch((err) => {
+				super.loadedData(event);
+			});
+	}
+
+	loadData(event?: any): void {
+		if (!this.formGroup.valid) {
+			let invalidControls = this.findInvalidControlsRecursive(this.formGroup);
+			const translationPromises = invalidControls.map((control) => this.env.translateResource(control));
+			Promise.all(translationPromises).then((values) => {
+				const invalidControls = values;
+				this.env.showMessage('Please recheck control(s): {{value}}', 'warning', invalidControls.join(' | '));
+			});
+			super.loadedData(event);
+			return;
 		}
-	}
-	dateMinusMonths(months: number): string {
-		const date = new Date();
-		date.setMonth(date.getMonth() - months);
-		return date.toISOString().split('T')[0]; // Format: YYYY-MM-DD
+		Object.assign(this.query, this.formGroup.getRawValue());
+
+		super.clearData();
+		super.loadData(event);
+		this.formGroup.markAsPristine();
+		this.pageConfig.isSubActive = true;
 	}
 
-	getCurrentDate(): string {
-		return new Date().toISOString().split('T')[0]; // Format: YYYY-MM-DD
-	}
+	onChangePeriodCategory() {
+		let postDTO = {
+			//IDBranch: this.formGroup.controls.IDBranch.value,
+			IgnoredBranch: true,
+			IDPeriodsCategory: this.formGroup.controls.IDPeriodCategory.value,
+		};
+		this.periodProvider
+			.read(postDTO)
+			.then((resp: any) => {
+				this.periodList = resp.data;
 
-	add() {
-		this.addLine({}, true);
+				this.loadData();
+			})
+			.catch((err) => {
+				this.env.showMessage(err, 'danger');
+			});
 	}
 }
