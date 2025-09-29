bc71bfaadcd7d68bfce8ad2c420c605c2218e66a
Hùng Vũ <hungvq@live.com>
2025-09-29 18:44:45 +0700

    clean code

--- goods-receiving/goods-receiving.module.ts
+++ goods-receiving/goods-receiving.module.ts
@@ -5,10 +5,9 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { GoodsReceivingPage } from './goods-receiving.page';
 import { ShareModule } from 'src/app/share.module';
-import { NgOptionHighlightModule } from '@ng-select/ng-option-highlight';
 
 @NgModule({
-	imports: [IonicModule, CommonModule, FormsModule, NgOptionHighlightModule, ShareModule, RouterModule.forChild([{ path: '', component: GoodsReceivingPage }])],
+	imports: [IonicModule, CommonModule, FormsModule, ShareModule, RouterModule.forChild([{ path: '', component: GoodsReceivingPage }])],
 	declarations: [GoodsReceivingPage],
 })
 export class GoodsReceivingPageModule {}
--- lpn-label/lpn-label.module.ts
+++ lpn-label/lpn-label.module.ts
@@ -6,7 +6,6 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { LPNLabelPage } from './lpn-label';
 import { ShareModule } from 'src/app/share.module';
 import { PipesModule } from 'src/app/pipes/pipes.module';
-import { NgOptionHighlightModule } from '@ng-select/ng-option-highlight';
 
 @NgModule({
 	imports: [CommonModule, FormsModule, IonicModule, ReactiveFormsModule, ShareModule, PipesModule, RouterModule.forChild([{ path: '', component: LPNLabelPage }])],
