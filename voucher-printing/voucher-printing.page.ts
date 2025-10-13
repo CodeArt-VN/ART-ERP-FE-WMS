a9935786495baf23d796d17a8b6e3b1a967b535b
bizyoh <bizyoh@gmail.com>
2025-10-13 16:38:11 +0700

    download voucher

--- voucher-printing/voucher-printing.page.ts
+++ voucher-printing/voucher-printing.page.ts
@@ -17,7 +17,7 @@ import * as JSZip from 'jszip';
 })
 export class VoucherPrintingPage extends PageBase {
 	initProgram = [];
-	lableConfig :any = {
+	lableConfig: any = {
 		PageWidth: 96,
 		QRCodeWidth: 206,
 		CodeFontSize: 15,
@@ -132,8 +132,8 @@ export class VoucherPrintingPage extends PageBase {
 				console.log(err);
 			});
 	}
-	resetToDefault(){
-		this.lableConfig = {PageWidth: 96, QRCodeWidth: 206, CodeFontSize: 15, NameFontSize: 9, NameLineClamp: 2};
+	resetToDefault() {
+		this.lableConfig = { PageWidth: 96, QRCodeWidth: 206, CodeFontSize: 15, NameFontSize: 9, NameLineClamp: 2 };
 		this.formGroup.get('PrintMode').setValue('Ruy96');
 	}
 	loadLabel() {
@@ -166,25 +166,88 @@ export class VoucherPrintingPage extends PageBase {
 		});
 	}
 
-	async downloadZip() {
-		const labels: any = await this.loadLabel();
-		const zip = new JSZip();
+	async downloadQrZip() {
+		if(!this.formGroup.controls.Code.value) return;
+		this.env.showLoading('Please wait for a few moments', this.loadLabel()).then((resp:any) => {
+		let labels = resp;
+		const encoder = new TextEncoder();
+		const chunks: BlobPart[] = [];
+
+		let offset = 0;
+		const centralDir: any[] = [];
 		let totalCount = 0;
-		for (const l of labels) {
+		for (const label of labels) {
 			totalCount++;
-			const base64Data = l.QRC.split(',')[1];
-			const binary = atob(base64Data);
-			const array = new Uint8Array(binary.length);
-			for (let i = 0; i < binary.length; i++) array[i] = binary.charCodeAt(i);
-			if (this.pageConfig.canViewCode) zip.file(`${l.Value}.png`, array);
-			else zip.file(`${totalCount}.png`, array);
+			const fileName = this.pageConfig.canViewCode ? `${label.Value}.png` : `${totalCount}.png`;
+			const fileBytes = Uint8Array.from(atob(label.QRC.split(',')[1]), c => c.charCodeAt(0));
+			const nameBytes = encoder.encode(fileName);
+
+			const crc = this.crc32(fileBytes);
+			const compSize = fileBytes.length;
+			const uncompSize = fileBytes.length;
+
+			// Local File Header
+			const header = new Uint8Array([
+				0x50, 0x4B, 0x03, 0x04, // PK..
+				20, 0, // version
+				0, 0,  // flags
+				0, 0,  // compression (0 = store)
+				0, 0, 0, 0, // time+date
+				crc & 0xff, (crc >> 8) & 0xff, (crc >> 16) & 0xff, (crc >> 24) & 0xff, // CRC
+				compSize & 0xff, (compSize >> 8) & 0xff, (compSize >> 16) & 0xff, (compSize >> 24) & 0xff,
+				uncompSize & 0xff, (uncompSize >> 8) & 0xff, (uncompSize >> 16) & 0xff, (uncompSize >> 24) & 0xff,
+				nameBytes.length, 0, // file name len
+				0, 0 // extra field len
+			]);
+
+			chunks.push(header, nameBytes, fileBytes);
+
+			const localHeaderOffset = offset;
+			offset += header.length + nameBytes.length + fileBytes.length;
+
+			// Central Directory entry
+			const cdir = new Uint8Array([
+				0x50, 0x4B, 0x01, 0x02, // PK..
+				20, 3, 20, 0, 0, 0, 0, 0,
+				0, 0, 0, 0,
+				compSize & 0xff, (compSize >> 8) & 0xff, (compSize >> 16) & 0xff, (compSize >> 24) & 0xff,
+				uncompSize & 0xff, (uncompSize >> 8) & 0xff, (uncompSize >> 16) & 0xff, (uncompSize >> 24) & 0xff,
+				nameBytes.length, 0, 0, 0, 0, 0,
+				0, 0, 0, 0, // disk num
+				0, 0, // int attr
+				0, 0, 0, 0, // ext attr
+				localHeaderOffset & 0xff, (localHeaderOffset >> 8) & 0xff, (localHeaderOffset >> 16) & 0xff, (localHeaderOffset >> 24) & 0xff
+			]);
+			centralDir.push({ cdir, nameBytes });
 		}
 
-		const blob = await zip.generateAsync({ type: 'blob' });
+		// Gộp các entry central dir
+		const centralDirStart = offset;
+		for (const { cdir, nameBytes } of centralDir) {
+			chunks.push(cdir, nameBytes);
+			offset += cdir.length + nameBytes.length;
+		}
+		const centralDirSize = offset - centralDirStart;
+
+		// EOCD (End of Central Directory)
+		const eocd = new Uint8Array([
+			0x50, 0x4B, 0x05, 0x06, // PK..
+			0, 0, 0, 0,
+			centralDir.length, 0,
+			centralDir.length, 0,
+			centralDirSize & 0xff, (centralDirSize >> 8) & 0xff, (centralDirSize >> 16) & 0xff, (centralDirSize >> 24) & 0xff,
+			centralDirStart & 0xff, (centralDirStart >> 8) & 0xff, (centralDirStart >> 16) & 0xff, (centralDirStart >> 24) & 0xff,
+			0, 0
+		]);
+		chunks.push(eocd);
+
+		// Gộp tất cả thành 1 Blob
+		const blob = new Blob(chunks, { type: 'application/zip' });
 		const url = URL.createObjectURL(blob);
+
+		const now = new Date();
 		const a = document.createElement('a');
 		a.href = url;
-		const now = new Date();
 		const formatted =
 			now.getFullYear() +
 			('0' + (now.getMonth() + 1)).slice(-2) +
@@ -194,5 +257,24 @@ export class VoucherPrintingPage extends PageBase {
 		a.download = `Labels_QR_${this.id}_${formatted}.zip`;
 		a.click();
 		URL.revokeObjectURL(url);
+		})
+	}
+	crc32(buf: Uint8Array) {
+		let crc = ~0;
+		const table = (() => {
+			const t = new Uint32Array(256);
+			for (let i = 0; i < 256; i++) {
+				let c = i;
+				for (let j = 0; j < 8; j++) {
+					c = (c & 1) ? (0xedb88320 ^ (c >>> 1)) : (c >>> 1);
+				}
+				t[i] = c >>> 0;
+			}
+			return t;
+		})();
+		for (let i = 0; i < buf.length; i++) {
+			crc = (crc >>> 8) ^ table[(crc ^ buf[i]) & 0xff];
+		}
+		return ~crc >>> 0;
 	}
 }
