89ed13c37059e7ea728c2ecbe1e81e43ee86b301
loipham <loipham21101998@gmail.com>
2026-04-22 17:12:15 +0700

    init receipt mobile page

---
+++ receipt-mobile-detail/receipt-mobile-detail.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+import { IonicModule } from '@ionic/angular';
+import { ShareModule } from 'src/app/share.module';
+import { ReceiptMobileDetailPage } from './receipt-mobile-detail.page';
+
+const routes: Routes = [
+	{
+		path: '',
+		component: ReceiptMobileDetailPage,
+	},
+];
+
+@NgModule({
+	imports: [CommonModule, FormsModule, IonicModule, ReactiveFormsModule, ShareModule, RouterModule.forChild(routes)],
+	declarations: [ReceiptMobileDetailPage],
+})
+export class ReceiptMobileDetailPageModule {}
